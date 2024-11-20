345c2619306ec7e85d481840bad1ce34910532a9
Javier Tausia Hoyal <tausiaj@unican.es>
2024-11-20 16:48:20 +0100

    [JTH] add version to test in lustre

--- bluemath_tk/downloaders/base_downloaders.py
+++ bluemath_tk/downloaders/base_downloaders.py
@@ -4,9 +4,18 @@ from ..core.logging import get_file_logger
 
 
 class BlueMathDownloader(ABC):
-    def __init__(self, base_path_to_download: str, debug: bool = False) -> None:
+    def __init__(
+        self, base_path_to_download: str, debug: bool = True, check: bool = False
+    ) -> None:
+        if not isinstance(base_path_to_download, str):
+            raise ValueError("base_path_to_download must be a string")
         self._base_path_to_download: str = base_path_to_download
+        if not isinstance(debug, bool):
+            raise ValueError("debug must be a boolean")
         self._debug: bool = debug
+        if not isinstance(check, bool):
+            raise ValueError("check must be a boolean")
+        self._check: bool = check
         self._logger: logging.Logger = get_file_logger(
             name="BlueMathDownloader",
             level=logging.INFO,
@@ -31,6 +40,14 @@ class BlueMathDownloader(ABC):
         self._debug = value
 
     @property
+    def check(self) -> bool:
+        return self._check
+
+    @check.setter
+    def check(self, value: bool) -> None:
+        self._check = value
+
+    @property
     def logger(self) -> logging.Logger:
         return self._logger
 
@@ -44,8 +61,4 @@ class BlueMathDownloader(ABC):
 
     @abstractmethod
     def download_data(self, *args, **kwargs) -> None:
-        pass
-
-    @abstractmethod
-    def check_data(self, *args, **kwargs) -> None:
         pass
--- bluemath_tk/downloaders/copernicus/copernicus_downloader.py
+++ bluemath_tk/downloaders/copernicus/copernicus_downloader.py
@@ -8,11 +8,29 @@ from ..base_downloaders import BlueMathDownloader
 
 config = {
     "url": "https://cds.climate.copernicus.eu/api",  # /v2?
-    "key": "5cf7efae-13fc-4085-8a98-80d82bdb55f5",
+    "key": "your-api-token",
 }
 
 
 class CopernicusDownloader(BlueMathDownloader):
+    """
+    This is the main class to download data from the Copernicus Climate Data Store.
+
+    Attributes
+    ----------
+    product : str
+        The product to download data from. Currently only ERA5 is supported.
+    product_config : dict
+        The configuration for the product to download data from.
+    client : cdsapi.Client
+        The client to interact with the Copernicus Climate Data Store API.
+
+    Notes
+    -----
+    - The configuration for the products is stored in the `products_configs` attribute.
+    - The configuration for the ERA5 product is stored in the `ERA5` folder.
+    """
+
     products_configs = {
         "ERA5": json.load(
             open(os.path.join(os.path.dirname(__file__), "ERA5", "ERA5_config.json"))
@@ -23,17 +41,47 @@ class CopernicusDownloader(BlueMathDownloader):
         self,
         product: str,
         base_path_to_download: str,
+        token: str = None,
         debug: bool = True,
+        check: bool = False,
     ) -> None:
-        super().__init__(base_path_to_download=base_path_to_download, debug=debug)
+        """
+        This is the constructor for the CopernicusDownloader class.
+
+        Parameters
+        ----------
+        product : str
+            The product to download data from. Currently only ERA5 is supported.
+        base_path_to_download : str
+            The base path to download the data to.
+        token : str, optional
+            The API token to use to download data. Defaults to None.
+        debug : bool, optional
+            Whether to run in debug mode. Defaults to True.
+        check : bool, optional
+            Whether to just check the data. Defaults to False.
+
+        Raises
+        ------
+        ValueError
+            If the product configuration is not found.
+        """
+
+        super().__init__(
+            base_path_to_download=base_path_to_download, debug=debug, check=check
+        )
         self._product = product
         self._product_config = self.products_configs.get(product)
         if self._product_config is None:
             raise ValueError(f"{product} configuration not found")
         self.set_logger_name(f"CopernicusDownloader - {product}")
-        # self._client = cdsapi.Client(
-        #     url=config["url"], key=config["key"], debug=self.debug
-        # )
+        if not self.check:
+            self._client = cdsapi.Client(
+                url=config["url"], key=token or config["key"], debug=self.debug
+            )
+            self.logger.info("---- DOWNLOADING DATA ----")
+        else:
+            self.logger.info("---- CHECKING DATA ----")
 
     @property
     def product(self) -> str:
@@ -47,19 +95,55 @@ class CopernicusDownloader(BlueMathDownloader):
     def client(self) -> cdsapi.Client:
         return self._client
 
-    def download_data(self, *args, **kwargs):
+    def download_data(self, *args, **kwargs) -> str:
+        """
+        Downloads the data for the product.
+
+        Parameters
+        ----------
+        *args
+            The arguments to pass to the download function.
+        **kwargs
+            The keyword arguments to pass to the download function.
+
+        Returns
+        -------
+        str
+            The message with the fully downloaded files and the not fully downloaded files.
+
+        Raises
+        ------
+        ValueError
+            If the product is not supported.
+        """
+
         if self.product == "ERA5":
             return self.download_data_era5(*args, **kwargs)
         else:
             raise ValueError(f"Download for product {self.product} not supported")
 
-    def check_data(self, *args, **kwargs):
-        return super().check_data(*args, **kwargs)
+    def list_variables(self) -> List[str]:
+        """
+        Lists the variables available for the product.
 
-    def list_variables(self):
+        Returns
+        -------
+        List[str]
+            The list of variables available for the product.
+        """
+
         return list(self.product_config["variables"].keys())
 
-    def list_datasets(self):
+    def list_datasets(self) -> List[str]:
+        """
+        Lists the datasets available for the product.
+
+        Returns
+        -------
+        List[str]
+            The list of datasets available for the product.
+        """
+
         return list(self.product_config["datasets"].keys())
 
     def download_data_era5(
@@ -73,9 +157,57 @@ class CopernicusDownloader(BlueMathDownloader):
         data_format: str = "netcdf",
         download_format: str = "unarchived",
         force: bool = False,
-    ):
-        if not isinstance(variables, list) or len(variables) == 0:
-            raise ValueError("Variables must be a non-empty list of strings")
+    ) -> str:
+        """
+        Downloads the data for the ERA5 product.
+
+        Parameters
+        ----------
+        variables : List[str]
+            The list of variables to download.
+        years : List[str]
+            The list of years to download.
+        months : List[str]
+            The list of months to download.
+        day : List[str], optional
+            The list of days to download. Defaults to None.
+        time : List[str], optional
+            The list of times to download. Defaults to None.
+        product_type : str, optional
+            The product type to download. Defaults to "reanalysis".
+        data_format : str, optional
+            The data format to download. Defaults to "netcdf".
+        download_format : str, optional
+            The download format to use. Defaults to "unarchived".
+        force : bool, optional
+            Whether to force the download. Defaults to False.
+
+        Returns
+        -------
+        str
+            The message with the fully downloaded files and the not fully downloaded files.
+
+        Raises
+        ------
+        ValueError
+            If the variables, years, months, day, time, product_type, data_format, or download_format are not valid
+            or if the force is not a boolean.
+
+        Notes
+        -----
+        - The variables, years, months, day, and time must be lists of strings.
+        - The product_type, data_format, and download_format must be strings.
+        - The force must be a boolean.
+        - Leave variables empty to download all variables.
+        - Leave day empty to download all days.
+        - Leave time empty to download all times.
+        """
+
+        if not isinstance(variables, list):
+            raise ValueError("Variables must be a list of strings")
+        elif len(variables) == 0:
+            variables = list(self.product_config["variables"].keys())
+            self.logger.info(f"Variables not provided. Using {variables}")
         if not isinstance(years, list) or len(years) == 0:
             raise ValueError("Years must be a non-empty list of strings")
         if not isinstance(months, list) or len(months) == 0:
@@ -101,9 +233,13 @@ class CopernicusDownloader(BlueMathDownloader):
         if not isinstance(force, bool):
             raise ValueError("Force must be a boolean")
 
+        fully_downloaded_files: List[str] = []
+        NOT_fullly_downloaded_files: List[str] = []
+
         for variable in variables:
             for year in years:
                 for month in months:
+                    month = f"{int(month):02d}"  # Ensure month is 2 digits
                     variable_config = self.product_config["variables"].get(variable)
                     if variable_config is None:
                         self.logger.error(
@@ -129,16 +265,20 @@ class CopernicusDownloader(BlueMathDownloader):
                     template_for_variable["data_format"] = data_format
                     template_for_variable["download_format"] = download_format
 
+                    skip_because_of_manadatory_fields = False
                     for mandatory_field in variable_dataset["mandatory_fields"]:
                         try:
                             if template_for_variable.get(mandatory_field) is None:
                                 template_for_variable[mandatory_field] = (
                                     variable_config[mandatory_field]
                                 )
-                        except KeyError as e:
-                            raise KeyError(
+                        except KeyError:
+                            self.logger.error(
                                 f"Mandotory field {mandatory_field} not found in variable configuration file for {variable}"
-                            ) from e
+                            )
+                            skip_because_of_manadatory_fields = True
+                    if skip_because_of_manadatory_fields:
+                        continue
 
                     # Create the output file name once request is properly formatted
                     output_nc_file = os.path.join(
@@ -154,36 +294,36 @@ class CopernicusDownloader(BlueMathDownloader):
                     # Create the output directory if it does not exist
                     os.makedirs(os.path.dirname(output_nc_file), exist_ok=True)
 
-                    continue
-
-                    self.logger.info(
-                        f"Downloading variable {variable}: {json.dumps(template_for_variable, indent=2)}"
-                    )
-                    self.logger.info(
-                        f"Variable info: {json.dumps(variable_config, indent=2)}"
-                    )
-
-                    if os.path.exists(output_nc_file):
-                        self.logger.debug(f"File {output_nc_file} exists")
-                        if not force:
-                            self.logger.debug("Checking the file is complete")
+                    if self.check or not force:
+                        if os.path.exists(output_nc_file):
+                            self.logger.debug(
+                                f"Checking {output_nc_file} file is complete"
+                            )
                             nc = xr.open_dataset(output_nc_file)
                             _, last_day = calendar.monthrange(int(year), int(month))
                             last_hour = f"{year}-{int(month):02d}-{last_day}T23"
-                            last_hour_nc = str(nc.time[-1].values)
+                            last_hour_nc = str(nc.valid_time[-1].values)
                             nc.close()
                             if last_hour not in last_hour_nc:
                                 self.logger.debug(
                                     f"{output_nc_file} ends at {last_hour_nc} instead of {last_hour}"
                                 )
-                                self.logger.debug(
-                                    f"Downloading: {variable} to {output_nc_file}"
-                                )
-                                self.client.retrieve(
-                                    name=variable_config["dataset"],
-                                    request=template_for_variable,
-                                    target=output_nc_file,
-                                )
+                                if self.check:
+                                    NOT_fullly_downloaded_files.append(output_nc_file)
+                                else:
+                                    self.logger.debug(
+                                        f"Downloading: {variable} to {output_nc_file} because it is not complete"
+                                    )
+                                    self.client.retrieve(
+                                        name=variable_config["dataset"],
+                                        request=template_for_variable,
+                                        target=output_nc_file,
+                                    )
+                                    fully_downloaded_files.append(output_nc_file)
+                            else:
+                                fully_downloaded_files.append(output_nc_file)
+                        elif self.check:
+                            NOT_fullly_downloaded_files.append(output_nc_file)
                         else:
                             self.logger.debug(
                                 f"Downloading: {variable} to {output_nc_file}"
@@ -193,6 +333,7 @@ class CopernicusDownloader(BlueMathDownloader):
                                 request=template_for_variable,
                                 target=output_nc_file,
                             )
+                            fully_downloaded_files.append(output_nc_file)
                     else:
                         self.logger.debug(
                             f"Downloading: {variable} to {output_nc_file}"
@@ -202,6 +343,12 @@ class CopernicusDownloader(BlueMathDownloader):
                             request=template_for_variable,
                             target=output_nc_file,
                         )
+                        fully_downloaded_files.append(output_nc_file)
+
+        return f"""
+            Fully downloaded files: {fully_downloaded_files}
+            Not fully downloaded files: {NOT_fullly_downloaded_files}
+        """
 
 
 if __name__ == "__main__":
@@ -209,6 +356,7 @@ if __name__ == "__main__":
         product="ERA5",
         base_path_to_download="/home/tausiaj/DATA/Copernicus/",
         debug=True,
+        check=False,
     )
     copernicus_downloader.download_data(
         variables=["geo500", "tp", "p140122"], years=["2021"], months=["01"]
