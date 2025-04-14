c5e3e969e239c5df45f59e8c7de44d72bdf03399
Javier Tausia Hoyal <tausiaj@unican.es>
2025-04-14 17:24:47 +0200

    [JTH] clean schism example

--- bluemath_tk/wrappers/schism/schism_wrapper.py
+++ bluemath_tk/wrappers/schism/schism_wrapper.py
@@ -1,3 +1,5 @@
+import os
+
 from .._base_wrappers import BaseModelWrapper
 
 
@@ -20,18 +22,20 @@ class SchismModelWrapper(BaseModelWrapper):
     def __init__(
         self,
         templates_dir: str,
-        model_parameters: dict,
+        metamodel_parameters: dict,
+        fixed_parameters: dict,
         output_dir: str,
         templates_name: dict = "all",
         debug: bool = True,
     ) -> None:
         """
-        Initialize the Delft3d model wrapper.
+        Initialize the Schism model wrapper.
         """
 
         super().__init__(
             templates_dir=templates_dir,
-            model_parameters=model_parameters,
+            metamodel_parameters=metamodel_parameters,
+            fixed_parameters=fixed_parameters,
             output_dir=output_dir,
             templates_name=templates_name,
             default_parameters=self.default_parameters,
@@ -39,3 +43,21 @@ class SchismModelWrapper(BaseModelWrapper):
         self.set_logger_name(
             name=self.__class__.__name__, level="DEBUG" if debug else "INFO"
         )
+
+    def build_case(
+        self,
+        case_context: dict,
+        case_dir: str,
+    ) -> None:
+        """
+        Build the input files for a case.
+
+        Parameters
+        ----------
+        case_context : dict
+            The case context.
+        case_dir : str
+            The case directory.
+        """
+
+        os.mkdir(os.path.join(case_dir, "outputs"), exist_ok=True)
