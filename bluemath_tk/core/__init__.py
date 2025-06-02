c7955a8d523b643d63d32971efa6bc6c89e82688
Javier Tausia Hoyal <tausiaj@unican.es>
2025-06-02 16:29:37 +0200

    [JTH] fill ALL __init__ files with metadata

--- bluemath_tk/core/__init__.py
+++ bluemath_tk/core/__init__.py
@@ -2,7 +2,6 @@
 Project: BlueMath_tk
 Sub-Module: core
 Author: GeoOcean Research Group, Universidad de Cantabria
-Creation Date: 9 December 2024
 Repository: https://github.com/GeoOcean/BlueMath_tk.git
 Status: Under development (Working)
 """
--- bluemath_tk/topo_bathy/__init__.py
+++ bluemath_tk/topo_bathy/__init__.py
@@ -2,7 +2,6 @@
 Project: BlueMath_tk
 Sub-Module: topo_bathy
 Author: GeoOcean Research Group, Universidad de Cantabria
-Creation Date: 19 April 2025
 Repository: https://github.com/GeoOcean/BlueMath_tk.git
 Status: Under development (Working)
 """
