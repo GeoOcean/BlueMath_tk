dea7fd81e185393dd4ced179d1148aad3746219e
Pablo Alonso <alonsoap@ui.sci.unican.es>
2025-06-03 13:17:20 +0200

    add build cases for apia (only jownsawp.txt)

--- bluemath_tk/wrappers/xbeach/xbeach_wrapper.py
+++ bluemath_tk/wrappers/xbeach/xbeach_wrapper.py
@@ -1,5 +1,5 @@
 from .._base_wrappers import BaseModelWrapper
-
+import math
 
 class XBeachModelWrapper(BaseModelWrapper):
     """
@@ -14,7 +14,18 @@ class XBeachModelWrapper(BaseModelWrapper):
         The available launchers for the wrapper.
     """
 
-    default_parameters = {}
+    default_parameters = {
+        "comptime": {
+            "type": int,
+            "value": 3600,
+            "description": "The computational time.",
+        },
+        "wbctype": {
+            "type": str,
+            "value": "off",
+            "description": "The time step for the simulation.",
+        },
+    }
 
     available_launchers = {
         "geoocean-cluster": "launchXbeach.sh",
@@ -44,3 +55,26 @@ class XBeachModelWrapper(BaseModelWrapper):
         self.set_logger_name(
             name=self.__class__.__name__, level="DEBUG" if debug else "INFO"
         )
+
+    def build_case(
+        self,
+        case_context: dict,
+        case_dir: str,
+    ) -> None:
+        """
+        Build the input files for a case.
+
+        Parameters
+        ----------
+        case_context : dict
+            The case context.
+        case_dir : str
+            The case directory.
+        """
+
+        if self.fixed_parameters['wbctype'] == 'jonstable':
+            with open(f"{case_dir}/jonswap.txt", "w") as f:
+                for i in range(math.ceil(self.fixed_parameters['comptime'] / 3600)):
+                    f.write(f"{case_context['Hs']} {case_context['Tp']} {case_context['Dir']} 3.300000 30.000000 3600.000000 1.000000 \n")
+        
+
